0b41deedd9164ef2d6bcd1dd2f047f2325e6024a
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-13 09:21:46 +0300

    commit tree 33e2300aa6109568abc99e87ac44a1ddd3330dcd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution here!
+let cats=["Milo", "Otis", "Garfield"]
+
+function initialValue(){
+    return ["Milo", "Otis", "Garfield"]
+}
+
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyAppendCat(){
+    return "Otis" 
+  }
